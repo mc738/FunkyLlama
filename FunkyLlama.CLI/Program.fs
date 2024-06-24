f27284c211f352c5638170dcc9f01e726c566f03
max <me@mclifford.dev>
2024-06-24 12:47:21 +0100

    Initial commit

---
+++ FunkyLlama.CLI/Program.fs
@@ -0,0 +1,21 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System
+open LLama.Common
+open LLama.Native
+
+module Test =
+    
+    let run () =
+        let modelPath = ""
+        
+        Console.ForegroundColor <- ConsoleColor.DarkGray
+        let modelParams = ModelParams(modelPath)
+        
+        
+        
+        
+        ()
+
+
+printfn "Hello from F#"
